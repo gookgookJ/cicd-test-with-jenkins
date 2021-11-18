e89d7e262b4ab31fef62991d36c7fdb6bee447af
hangoook <hangook@hangoookui-MacBookPro.local>
2021-11-18 12:46:56 +0900

    ci/cd test

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+const server = app.listen(3000, function () {
+  let port = server.address().port;
+
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+module.exports = app;
