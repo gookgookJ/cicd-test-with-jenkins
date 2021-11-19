45aeeee24448b4422ac05c65fa42817899d16ed0
hangoook <hangook@hangoookui-MacBookPro.local>
2021-11-19 09:49:11 +0900

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!!!');
 });
 
-const server = app.listen(3000, function () {
+const server = app.listen(4000, function () {
   let port = server.address().port;
 
   console.log(`App listening at http://localhost:${port}`);
